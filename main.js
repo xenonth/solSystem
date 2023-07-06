889ed8bfd3e9ccf9c0a68c098315a0f4f5d78181
xenonth <timothy.forbes259@gmail.com>
2023-07-06 14:23:39 +0930

    created scene initailiser class for new threejs projects and imported.

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 import * as THREE from 'three';
+import SceneInit from "./modules/SceneInit";
+
+let solSystem = new SceneInit();
+
+solSystem.initScene();
+solSystem.animate();
--- modules/SceneInit.js
+++ modules/SceneInit.js
@@ -21,5 +21,54 @@ export default class SceneInit {
             1000
         );
         this.camera.position.z = 128;
+
+        this.scene = new THREE.Scene();
+        //const starBackground = new THREE.TextureLoader.load('../images/background.jpg');
+        // this.scene.background = starBackground;
+
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: document.getElementById("mySolSystem"),
+            antialias: true,
+        });
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+
+        this.controls = new OrbitControls(this.camera,  this.renderer.domElement);
+
+        this.Stats = Stats();
+        document.appendChild(this.stats.dom);
+
+        //ambient light to light up the whole scene
+        // let ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+        //ambientLight.castShadow = false;
+        //this.scene.add(ambientLight);
+
+        //spotlight illuminating the chart directly
+        //let spotLight = new Three.spotLight(0xffffff, 0.7);
+        //spotLight.castShadow = false;
+        //spotLight.position.set(0, 40,0);
+        //this.scene.add(spotLight);
+
+        //if window rezizes
+        window.addEventListener("resize", () => this.onWindowResize(), false);
+    }
+    
+    //animating the scene
+    animate() {
+        window.requestAnimationFrame(this.animate.bind(this));
+        this.render();
+        this.stats.update();
+        //this.controls.update();
+    }
+
+    //rendering the scene
+    render() {
+        this.renderer.render(this.scene, this.camera)
+    }
+
+    onWindowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 }
