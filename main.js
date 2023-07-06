c16fa4f8fb43d9150801b71b3fe0539278f735d8
xenonth <timothy.forbes259@gmail.com>
2023-07-06 15:20:31 +0930

    Able to render sun to the screen.

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 import * as THREE from 'three';
 import SceneInit from "./modules/SceneInit";
+import Planet from './modules/Planet';
 
 let solSystem = new SceneInit();
 
 solSystem.initScene();
 solSystem.animate();
+
+//variables to create sungeometry
+const sunGeometry = new THREE.SphereGeometry(8);
+const sunTexture = new THREE.TextureLoader().load("./images/sun.jpg");
+const sunMaterial = new THREE.MeshBasicMaterial({map: sunTexture});
+const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
+
+const solarSystem = new THREE.Group();
+solarSystem.add(sunMesh);
+solSystem.scene.add(solarSystem);
